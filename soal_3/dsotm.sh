8adeade53e8a28e7e7d8e326ff6377d91ca06e2d
rafi-adly <rafiadly259@gmail.com>
2025-03-15 11:16:57 +0700

    Update dsotm.sh

---
+++ soal_3/dsotm.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+SpeakToMe() {
+        clear
+        while true; do
+                curl -s "Accept: application/json" "https://www.affirmations.dev" | jq -r '.affirmation'
+                sleep 2
+        done
+}
+
+OnTheRun() {
+        clear
+        echo -n "Progress: ["
+        for i in $(seq 1 50); do
+                sleep $(awk -v min=0.1 -v max=1 'BEGIN{srand(); print min+rand()*(max-min)}')
+                echo -n "#"
+        done
+        echo "] Done!"
+}
+
+Time() {
+        while true; do
+                clear
+                echo "ᒍᗩᗰ ᗷᕮᖇᗩᑭᗩ"
+                echo "====================================================="
+                echo " "
+                date +"%Y-%m-%d %H:%M:%S"
+                sleep 1
+        done
+}
+
+Money() {
+        clear
+        chars="$€£¥¢₹₩₿₣"
+        cols=$(tput cols)
+        rows=$(tput lines)
+        while true; do
+                col=$((RANDOM % cols))
+                char=${chars:RANDOM%${#chars}:1}
+                tput cup $((RANDOM % rows)) $col
+                echo -e "\e[1;32m$char\e[0m"
+                sleep 0.07
+        done
+}
+
+BrainDmg() {
+        clear
+        while true; do
+                ps -eo pid,comm --sort=-%mem | head -n 10
+                sleep 1
+        done
+}
+
+case "$1" in
+        "--play=Speak to Me")
+                SpeakToMe
+                ;;
+        "--play=On the Run")
+                OnTheRun
+                ;;
+        "--play=Time")
+                Time
+                ;;
+        "--play=Money")
+                Money
+                ;;
+        "--play=Brain Damage")
+                BrainDmg
+                ;;
+        *)
+                echo "Pakai format yang benar --> $0 --play=\"<Track>\""
+                ;;
+esac
